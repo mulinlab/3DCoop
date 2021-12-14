fe83cd66481935ff4adfbc1f30af6365460723b6
Yixf(Yi xianfu) <yixianfu@tmu.edu.cn>
2021-12-14 16:04:55 +0800

    Add samtools in conda

--- conda.sh
+++ conda.sh
@@ -5,6 +5,6 @@ conda create -n 3DCoop
 conda activate 3DCoop
 
 # conda install perl-db-file bedtools
-conda install bedtools perl-list-moreutils perl-parallel-forkmanager r-tidyverse r-reshape r-huge r-igraph r-desctools r-ggnetwork r-intergraph
+conda install bedtools samtools perl-list-moreutils perl-parallel-forkmanager r-tidyverse r-reshape r-huge r-igraph r-desctools r-ggnetwork r-intergraph
 
 conda deactivate 3DCoop
